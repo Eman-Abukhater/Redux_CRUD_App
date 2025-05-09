26a71447b1d56c1e9b62efe6d24e62eae422385b
Eman <emanabukhater2001@gmail.com>
2025-05-09 17:42:41 +0300

    Fetch and Display Users in Users List

--- redux-crud-app/src/components/UserList.jsx
+++ redux-crud-app/src/components/UserList.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { deleteUser } from "../users/usersSlice";
+import { deleteUser, fetchUsers} from "../users/usersSlice";
 import {
   List,
   ListItem,
@@ -9,22 +9,45 @@ import {
   Typography,
   Box,
   Paper,
+  CircularProgress,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 const UserList = () => {
-  const users = useSelector((state) => state.users.users); // Read from Redux
+  const { users, loading, error } = useSelector((state) => state.users);
   const dispatch = useDispatch();
+  // Fetch users when the component mounts
+  useEffect(() => {
+    dispatch(fetchUsers()); // Dispatch the fetchUsers action when the component mounts
+  }, [dispatch]);
 
+  // Handle delete user action
   const handleDelete = (id) => {
     dispatch(deleteUser(id)); // Send delete action
   };
 
+  if (loading) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh", // Full viewport height
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+      }
+
+  if (error) {
+    return <Typography color="error">{error}</Typography>; // Show error message if something goes wrong
+  }
+
   return (
     <Paper elevation={3} sx={{ padding: 2 }}>
-      <Typography variant="h5" >
-        User List
-      </Typography>
+      <Typography variant="h5">User List</Typography>
 
       {users.length === 0 ? (
         <Typography>No users yet.</Typography>
